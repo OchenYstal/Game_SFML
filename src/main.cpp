3b946bf1003c71302e9d630830af008a25df418c
OchenYstal <Ystal242@gmail.com>
2025-09-17 18:22:09 +0300

    SFML Game: move display fps

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <math.h>
 #include <iostream>
+#include <string>
 
 #include "Player.hpp"
 #include "Enemy.hpp"
@@ -8,7 +9,7 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({1920, 1080}), "SFML works!", sf::Style::Titlebar);
-    window.setFramerateLimit(240);
+    window.setFramerateLimit(60);
 
     Player player;
     Enemy enemy;
@@ -19,12 +20,24 @@ int main()
     player.Load();
     enemy.Load();
 
+    sf::Font font;
+    if(!font.openFromFile("assets/Fonts/Font1FPS.ttf"))
+    {
+        std::cout << "Fail" << std::endl;   
+    }
+    sf::Text FrameRateText(font);
+    FrameRateText.setCharacterSize(18);
     sf::Clock clock;
 
     while (window.isOpen())
     {
     sf::Time DeltaTimeTimer = clock.restart(); 
     float DeltaTime = DeltaTimeTimer.asMilliseconds(); 
+    float Result = 1000 / DeltaTime;
+
+    std::string frameRateText = "FPS: "  + std::to_string((int)Result) + " FrameTime: " + std::to_string((int)DeltaTime);
+    FrameRateText.setString(frameRateText);
+
         //-------------- UPDATE ------------
         while (const std::optional event = window.pollEvent())
         {
@@ -38,6 +51,7 @@ int main()
 //-------------- UPDATE ------------
 //-------------- DRAW ------------
         window.clear(); 
+        window.draw(FrameRateText);
         player.Draw(window);
         enemy.Draw(window);
         window.display();
